616a5efaa57365e5172723314ba1c732de5040f8
ShiftyKitty <cathalpmcgahan@gmail.com>
2025-07-31 14:13:14 +0100

    build: build InitialCreate, InitialCreateDesigner and DbContextModelSnapshot

---
+++ src/TaskManagement.API/Migrations/20250731124632_InitialCreate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TaskManagement.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Priority = table.Column<int>(type: "INTEGER", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tasks",
+                columns: new[] { "Id", "Description", "DueDate", "Priority", "Status", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Update the system to version 2.0 with all the new features", new DateTime(2025, 8, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, 0, "Deploy new version of the application" },
+                    { 2, "Complete the updated API documentation for version 2.0", new DateTime(2025, 8, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, 1, "Update API documentation" },
+                    { 3, "Investigate new libraries for potential inclusion in the project", new DateTime(2025, 7, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), 0, 2, "Research new libraries" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
