fc5784e4dd66ab570459738ef4d8fc008541828f
ShiftyKitty <cathalpmcgahan@gmail.com>
2025-08-03 12:55:31 +0100

    fix: Resolved react package mismanagement causing multiple react libraries to be used in project

--- tests/TaskManagement.Web.Tests/jest.config.js
+++ tests/TaskManagement.Web.Tests/jest.config.js
@@ -1,10 +1,14 @@
+const path = require('path');
+
 module.exports = {
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(js|jsx)$': 'babel-jest'
   },
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy', 
+    '^react$': path.resolve(__dirname, '../../src/TaskManagement.Web/node_modules/react'),
+    '^react-dom$': path.resolve(__dirname, '../../src/TaskManagement.Web/node_modules/react-dom'),
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js'
   },
   setupFilesAfterEnv: ['<rootDir>/setupJest.js'],
