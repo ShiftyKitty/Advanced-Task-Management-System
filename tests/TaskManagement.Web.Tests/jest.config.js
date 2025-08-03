42a8c3e19704308c7b939520c7e093843ef773b2
ShiftyKitty <cathalpmcgahan@gmail.com>
2025-08-03 10:43:20 +0100

    test: Updated config to better address css testing issues

--- tests/TaskManagement.Web.Tests/jest.config.js
+++ tests/TaskManagement.Web.Tests/jest.config.js
@@ -1,11 +1,10 @@
-// tests/TaskManagement.Web.Tests/jest.config.js
 module.exports = {
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(js|jsx)$': 'babel-jest'
   },
   moduleNameMapper: {
-    '\\.css$': 'identity-obj-proxy',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy', 
     '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js'
   },
   setupFilesAfterEnv: ['<rootDir>/setupJest.js'],
