c50aae7483899b0431cf5c7b6c3130c0f219f497
ShiftyKitty <cathalpmcgahan@gmail.com>
2025-08-03 10:20:49 +0100

    test: Added jest config and set up files to tests\TaskManagement.Web.Tests dir

---
+++ tests/TaskManagement.Web.Tests/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    // Handle CSS imports (with CSS modules)
+    '\\.css$': 'identity-obj-proxy',
+    // Handle image imports
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js'
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['/node_modules/']
+};
