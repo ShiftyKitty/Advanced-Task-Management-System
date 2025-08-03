bbcd2224ada66139374c20453627ca18c7276688
ShiftyKitty <cathalpmcgahan@gmail.com>
2025-08-03 10:32:08 +0100

    test: updated jest config to resolve test errors

--- tests/TaskManagement.Web.Tests/jest.config.js
+++ tests/TaskManagement.Web.Tests/jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  },
   moduleNameMapper: {
-    // Handle CSS imports (with CSS modules)
     '\\.css$': 'identity-obj-proxy',
-    // Handle image imports
     '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js'
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
